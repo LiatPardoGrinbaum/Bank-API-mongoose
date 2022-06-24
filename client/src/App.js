f9f1532cfd676a4c23b5e60aa3a703a9de58ead5
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-06-24 23:28:14 +0300

    install react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { useState } from "react";
+import { userAPI } from "./api/user.api";
+function App() {
+  const [user, setUser] = useState(null);
+  const handleclick = async () => {
+    const { data } = await userAPI.get("/users");
+    console.log(data);
+  };
+  return (
+    <div className="App">
+      <button onClick={handleclick}>Get all users</button>
+    </div>
+  );
+}
+
+export default App;
