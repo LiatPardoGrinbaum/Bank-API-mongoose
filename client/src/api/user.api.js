84da72dc46b2c80078021220b66c74622fc7a21a
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-06-25 00:05:52 +0300

    heroku client upload

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -1,4 +1,11 @@
 import axios from "axios";
+
+let url = "http://localhost:5050/api";
+
+if (process.env.NODE_ENV === "production") {
+  url = "/api";
+  //i'm already on my root in heroku so i don't need all the address
+}
 export const userAPI = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: url,
 });
--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 import express from "express";
 import { userRouter } from "./routes/user.routes.js";
-
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
+
+const fileName = fileURLToPath(import.meta.url);
+const __diranme = path.dirname(fileName);
+const publicDirPath = path.join(__diranme, "../client/build");
+// console.log(publicDirPath);
+app.use(cors());
+app.use(express.static(publicDirPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+});
 
 //for post and put
 app.use(express.json());
