faa6b011973ca4b2693aa3b1ea0bcfda5273825c
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-06-25 00:50:06 +0300

    *

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const app = express();
 const fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileName);
 const publicDirPath = path.join(__dirname, "../client/build");
-// console.log(publicDirPath);
+console.log(publicDirPath);
 app.use(cors());
 app.use(express.static(publicDirPath));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
 
